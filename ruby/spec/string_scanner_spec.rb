8c608ef2c69463aa5faa60cffb036bfab6de8a4e
bootstraponline <code@bootstraponline.com>
2017-11-20 12:36:29 -0500

    Fix quotes

--- ruby/spec/string_scanner_spec.rb
+++ ruby/spec/string_scanner_spec.rb
@@ -48,7 +48,7 @@ describe 'StringScanner' do
   end
 
   it '#charPos' do
-    s = StringScanner.new("abcädeföghi")
+    s = StringScanner.new('abcädeföghi')
     expect(s.charpos).to eq(0)
     expect(s.scan_until(/ä/)).to eq('abcä')
     expect(s.pos).to eq(5)
@@ -56,7 +56,7 @@ describe 'StringScanner' do
   end
 
   it '#charPos - debug' do
-    s = StringScanner.new("ebcadefoghi")
+    s = StringScanner.new('ebcadefoghi')
     expect(s.charpos).to eq(0)
     expect(s.scan_until(/a/)).to eq('ebca')
     expect(s.pos).to eq(4)
@@ -105,7 +105,7 @@ describe 'StringScanner' do
   end
 
   it '#check' do
-    s = StringScanner.new("Fri Dec 12 1975 14:39")
+    s = StringScanner.new('Fri Dec 12 1975 14:39')
     expect(s.check(/Fri/)).to eq('Fri')
     expect(s.pos).to eq(0)
     expect(s.matched).to eq('Fri')
@@ -114,14 +114,14 @@ describe 'StringScanner' do
   end
 
   it '#scan_until' do
-    s = StringScanner.new("Fri Dec 12 1975 14:39")
+    s = StringScanner.new('Fri Dec 12 1975 14:39')
     expect(s.scan_until(/1/)).to eq('Fri Dec 1')
     expect(s.pre_match).to eq('Fri Dec ')
     expect(s.scan_until(/XYZ/)).to eq(nil)
   end
 
   it '#skip_until' do
-    s = StringScanner.new("Fri Dec 12 1975 14:39")
+    s = StringScanner.new('Fri Dec 12 1975 14:39')
     expect(s.skip_until /12/).to eq(10)
   end
 
@@ -134,20 +134,20 @@ describe 'StringScanner' do
   end
 
   it '#check_until' do
-    s = StringScanner.new("Fri Dec 12 1975 14:39")
+    s = StringScanner.new('Fri Dec 12 1975 14:39')
     expect(s.check_until(/12/)).to eq('Fri Dec 12')
     expect(s.pos).to eq(0)
     expect(s.matched).to eq('12') # bug in strscan.c -- 12 is a number when it should be a string.
   end
 
   it '#getch' do
-    s = StringScanner.new("ab")
+    s = StringScanner.new('ab')
     expect(s.getch).to eq('a')
     expect(s.getch).to eq('b')
     expect(s.getch).to eq(nil)
 
     s = StringScanner.new("\244\242")
-    # strscan.c s.getch # => "\244\242" doesn't work on Ruby 2.3
+    # strscan.c s.getch # => '\244\242' doesn't work on Ruby 2.3
     # instead the characters are returned one at a time.
     # probably another bug in the example.
     expect(s.getch).to eq("\xA4")
@@ -156,7 +156,7 @@ describe 'StringScanner' do
   end
 
   it '#get_byte' do
-    s = StringScanner.new("ab")
+    s = StringScanner.new('ab')
     expect(s.get_byte).to eq('a')
     expect(s.get_byte).to eq('b')
     expect(s.get_byte).to eq(nil)
@@ -169,8 +169,8 @@ describe 'StringScanner' do
 
   it '#peek' do
     s = StringScanner.new('test string')
-    expect(s.peek(7)).to eq("test st")
-    expect(s.peek(7)).to eq("test st")
+    expect(s.peek(7)).to eq('test st')
+    expect(s.peek(7)).to eq('test st')
   end
 
   it '#unscan' do
@@ -226,12 +226,47 @@ describe 'StringScanner' do
     expect(s.match?(/\w+/)).to eq(4)
     expect(s.matched).to eq('test')
   end
-  
+
   it '#matched_size' do
     s = StringScanner.new('test string')
     expect(s.check(/\w+/)).to eq('test')
     expect(s.matched_size).to eq(4)
     expect(s.check(/\d+/)).to eq(nil)
     expect(s.matched_size).to eq(nil)
+  end
+
+  it '#nthSubgroup' do
+    s = StringScanner.new('Fri Dec 12 1975 14:39')
+    expect(s.scan(/(\w+) (\w+) (\d+) /)).to eq('Fri Dec 12 ')
+    expect(s[0]).to eq('Fri Dec 12 ')
+    expect(s[1]).to eq('Fri')
+    expect(s[2]).to eq('Dec')
+    expect(s[3]).to eq('12')
+    expect(s.post_match).to eq('1975 14:39')
+    expect(s.pre_match).to eq('')
+
+    s.reset
+    expect(s.scan(/(?<wday>\w+) (?<month>\w+) (?<day>\d+) /)).to eq('Fri Dec 12 ')
+    expect(s[0]).to eq('Fri Dec 12 ')
+    expect(s[1]).to eq('Fri')
+    expect(s[2]).to eq('Dec')
+    expect(s[3]).to eq('12')
+    expect(s[:wday]).to eq('Fri')
+    expect(s[:month]).to eq('Dec')
+    expect(s[:day]).to eq('12')
+    expect(s.post_match).to eq('1975 14:39')
+    expect(s.pre_match).to eq('')
+  end
+
+  it '#pre_match' do
+    s = StringScanner.new('test string')
+    expect(s.scan(/\w+/)).to eq('test')
+    expect(s.scan(/\s+/)).to eq(' ')
+    expect(s.pre_match).to eq('test')
+    expect(s.post_match).to eq('string')
+  end
+
+  it '#post_match' do
+
   end
 end
